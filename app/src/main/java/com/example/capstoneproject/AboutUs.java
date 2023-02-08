16563fa71a9606a3551354c5e0c97c2bcb8286c2
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2023-02-08 12:42:30 -0700

    AboutUs Working5 -Jiyeong

--- app/src/main/java/com/example/capstoneproject/AboutUs.java
+++ app/src/main/java/com/example/capstoneproject/AboutUs.java
@@ -2,7 +2,10 @@ package com.example.capstoneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+    }
+    private void goToUrl(String url){
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+    public void goToTwitter (View view){
+        goToUrl("https://twitter.com/WinnipegProper1");
+    }
+
+    public void goToFacebook (View view){
+        goToUrl("https://www.facebook.com/winnipegpropertymanagement/");
+    }
+
+    public void goToYoutube (View view){
+        goToUrl("https://www.youtube.com/channel/UCRs3SSmwVFbR2QEjIDqU0Ug");
+    }
+
+    public void goToLinkIn (View view){
+        goToUrl("https://www.linkedin.com/company/garamark-property-management/");
     }
 }
