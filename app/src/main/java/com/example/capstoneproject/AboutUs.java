0a73c4ad71451352bd5ae219cae78fdf8e97b49a
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2023-03-01 12:53:37 -0700

    Add comment and Ask Question page5 -Jiyeong

--- app/src/main/java/com/example/capstoneproject/AboutUs.java
+++ app/src/main/java/com/example/capstoneproject/AboutUs.java
@@ -14,23 +14,29 @@ public class AboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
     }
+
+    //This function links to string value of url.
     private void goToUrl(String url){
         Uri uriUrl = Uri.parse(url);
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
     }
+    //This function go to twitter url.
     public void goToTwitter (View view){
         goToUrl("https://twitter.com/WinnipegProper1");
     }
 
+    //This function go to facebook url.
     public void goToFacebook (View view){
         goToUrl("https://www.facebook.com/winnipegpropertymanagement/");
     }
 
+    //This function go to youtube url.
     public void goToYoutube (View view){
         goToUrl("https://www.youtube.com/channel/UCRs3SSmwVFbR2QEjIDqU0Ug");
     }
 
+    //This function go to linkedin url.
     public void goToLinkIn (View view){
         goToUrl("https://www.linkedin.com/company/garamark-property-management/");
     }
