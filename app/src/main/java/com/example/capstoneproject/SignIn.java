fca131d122ef480650e9be265f7b78fe699ad370
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2023-03-03 16:18:42 -0700

    Some fix 3-Jiyeong

--- app/src/main/java/com/example/capstoneproject/SignIn.java
+++ app/src/main/java/com/example/capstoneproject/SignIn.java
@@ -1,19 +1,21 @@
 package com.example.capstoneproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignIn extends Activity {
 
-    public SignIn() {
-    }
+    TextView tv_register;
+    LinearLayout lv_login;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,13 +23,33 @@ public class SignIn extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        //_bg__iphone_14___2_ek2 = (View) findViewById(R.id._bg__iphone_14___2_ek2);
+        tv_register = findViewById(R.id.tv_register);
+        lv_login = findViewById(R.id.lv_login);
+
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        lv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
 
 
 }
+
+
+
 
 
 
