322785010e932b16600b626233c758db6387f897
Thelaxman <gusainlakshman@gmail.com>
2023-03-12 23:59:07 -0600

    errors fixed

--- app/src/main/java/com/example/capstoneproject/MainActivity.java
+++ app/src/main/java/com/example/capstoneproject/MainActivity.java
@@ -1,20 +1,10 @@
 package com.example.capstoneproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-
-
-
-
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
     public void signup(View view) {
         // build an intent to open another activity page
         Intent intent = new Intent(this, SignUp.class);
